cbed4566be10e59412ce9c51ff93a47c177a7610
Arwa <arwa.it15@gmail.com>
2017-06-20 23:03:12 -0500

    create eventListeners for checkbox and toggle on it

---
+++ javascript/styling.js
@@ -0,0 +1,18 @@
+let darkThemeCheckbox = document.getElementById('darkTheme');
+let largeTextCheckbox = document.getElementById('largeText');
+let mainDiv = document.getElementById('pageWrapper');
+let buttons = document.getElementsByTagName('button');
+darkThemeCheckbox.addEventListener('change', function()
+{
+	mainDiv.classList.toggle('theme');
+})
+
+largeTextCheckbox.addEventListener('change', function()
+{
+	mainDiv.classList.toggle('largeText');
+	for(let i=0; i<buttons.length; i++)
+		{
+			buttons[i].classList.toggle('buttonText');
+		}
+	
+})
