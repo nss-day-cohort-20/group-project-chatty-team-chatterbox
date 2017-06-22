621003f478e749652da90b8a5c423fd3d03b1cd7
Greta Moseley <greta.moseley@gmail.com>
2017-06-22 10:30:49 -0500

    add attempt to get value from radio buttons

---
+++ javascript/modal.js
@@ -0,0 +1,36 @@
+
+
+let blue=document.getElementById("blue");
+let green=document.getElementById("green");
+let purple=document.getElementById("purple");
+let black=document.getElementById("black");
+
+let white=document.getElementById("white");
+let yellow=document.getElementById("yellow");
+let lime=document.getElementById("lime");
+let pink =document.getElementById("pink");
+
+//click handler
+
+// blue.addEventListener("click", function(event){
+// 	console.log ("event",event);
+// 	console.log ("blue", blue.value);
+// })
+
+function getValue(){
+
+		let blueClick= blue.value ;
+		console.log ("blueClick", blueClick);
+		return blueClick;
+	}
+
+
+document.getElementById("saveBtn").addEventListener("click", getValue){
+
+	let blue=getValue();
+
+    if (blue.checked===true){
+     mainDiv.classList.toggle("blue");
+        console.log("You got it!")
+    }
+}
